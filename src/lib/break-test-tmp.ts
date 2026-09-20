a53e27e5737d5eea83009984fbd6fd011d428df8
Drew Mattick <Dramattick1@gmail.com>
2026-09-19 21:49:40 -0500

    break-test: remove lint break, flip a test assertion (expect unit tests red)

--- src/lib/break-test-tmp.ts
+++
@@ -1,6 +0,0 @@
-// break-test: prefer-const is an ESLint ERROR (not warning) in this
-// config; breakTestVal is never reassigned.
-export function breakTestNoop(): number {
-  let breakTestVal = 1;
-  return breakTestVal;
-}
